fbe6031ef1303156c63b5c1ee76ed4536aa92dbd
Mujeeb4582 <mujeeburahman4582@gmail.com>
2022-08-25 16:35:40 +0500

    added the solution of staircaseproblem

---
+++ stairCaseProblemStatment.js
@@ -0,0 +1,18 @@
+console.log("StairCase solution");
+function staircase(n){
+    // first loop upto n
+    // decleare a string and store the pattren
+    for(let i = 0; i < n; i++){
+        let pattren = "";
+        for(let j = 0; j < n-1-i; j++){
+            pattren += " ";
+        }
+        for(let k = 0; k <= i; k++){
+            pattren += "#";
+        }
+        console.log(pattren);
+    }
+}
+
+let number = 6;
+staircase(6);
